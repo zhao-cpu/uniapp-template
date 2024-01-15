f9a7951137602932b6d8677dc8ff633c68c1b3f2
赵先生 <15675521581@163.com>
2024-01-15 22:36:24 +0800

    feat: 分包和预下载配置

---
+++ src/member/setting/setting.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="setting">setting</view>
+</template>
+
+<script lang="ts" setup>
+onLoad(e => {
+  console.log(e);
+});
+
+const props = defineProps<{ id: number }>();
+console.log('🚀 ~ props:', props);
+</script>
+
+<style lang="scss" scoped></style>
--- src/pages/mine/mine.vue
+++ src/pages/mine/mine.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="text-primary">mine</div>
+  <div class="text-primary" @tap="handleLink">mine</div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+function handleLink() {
+  uni.navigateTo({ url: '/member/setting/setting?id=1' });
+}
+</script>
 
 <style lang="scss" scoped></style>
